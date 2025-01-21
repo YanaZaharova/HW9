67ce4f4fcddd65dcfef5d68e5147da1fc7fce9fb
Zaharova Yana <yanamelomano@mail.ru>
2025-01-21 18:05:00 +0300

    ChangeTests

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,4 +1,5 @@
 package ru.netology;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +18,31 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSwitchToNext() {
+    public void shouldNotSetSNAboveMax() {
+        Radio station = new Radio();
+
+        station.setCurrentStationNumber(10);
+
+        int expected = 0;
+        int actual = station.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetSNBelowMin() {
+        Radio station = new Radio();
+
+        station.setCurrentStationNumber(-1);
+
+        int expected = 0;
+        int actual = station.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSwitchToNextAboveMax() {
         Radio station = new Radio();
 
         station.setCurrentStationNumber(9);
@@ -31,7 +56,21 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSwitchToPrev() {
+    public void shouldSwitchToNext() {
+        Radio station = new Radio();
+
+        station.setCurrentStationNumber(4);
+
+        station.next();
+
+        int expected = 5;
+        int actual = station.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSwitchToPrevIfMin() {
         Radio station = new Radio();
 
         station.setCurrentStationNumber(0);
@@ -40,6 +79,44 @@ public class RadioTest {
 
         int expected = 9;
         int actual = station.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSwitchToPrev() {
+        Radio station = new Radio();
+
+        station.setCurrentStationNumber(4);
+
+        station.prev();
+
+        int expected = 3;
+        int actual = station.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolumeAboveMax() {
+        Radio vol = new Radio();
+
+        vol.setCurrentVolume(101);
+
+        int expected = 0;
+        int actual = vol.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolumeBelowMin() {
+        Radio vol = new Radio();
+
+        vol.setCurrentVolume(-1);
+
+        int expected = 0;
+        int actual = vol.getCurrentVolume();
 
         Assertions.assertEquals(expected, actual);
     }
@@ -59,6 +136,20 @@ public class RadioTest {
     }
 
     @Test
+    public void shouldNotIncreaseAboveMax() {
+        Radio vol = new Radio();
+
+        vol.setCurrentVolume(100);
+
+        vol.increaseVolume();
+
+        int expected = 100;
+        int actual = vol.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void shouldTurnDownVolume() {
         Radio vol = new Radio();
 
@@ -67,6 +158,20 @@ public class RadioTest {
         vol.turnDownVolume();
 
         int expected = 49;
+        int actual = vol.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotTurnDownIfMin() {
+        Radio vol = new Radio();
+
+        vol.setCurrentVolume(0);
+
+        vol.turnDownVolume();
+
+        int expected = 0;
         int actual = vol.getCurrentVolume();
 
         Assertions.assertEquals(expected, actual);
